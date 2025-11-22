2493f12ae45de662fef1735f2020190fe6899b35
DiNeves <dms.neves@gmail.com>
2025-11-22 00:47:15 +0000

    Create tests for store page navigation between tabs

---
+++ tests/data/store.data.js
@@ -0,0 +1,65 @@
+export const STORELABELS = {
+
+    home: {
+        pageTitle: 'Instructions'
+    },
+
+    inventory: {
+        pageTitle: 'Inventory Management'
+    },
+
+    catalog: {
+        pageTitle: 'Product Catalog'
+    },
+
+    cart: {
+        pageTitle: 'Your Cart'
+    },
+
+    payments: {
+        pageTitle: 'Payment'
+    },
+
+    orders: {
+        pageTitle: 'Purchase Orders'
+    }
+
+};
+
+export const STORELOCATORS = {
+
+    goTo: {
+        storeUrl: '/store'
+    },
+
+    home: {
+        tab: 'store-tab-home',
+        title: 'instructions-title'
+    },
+
+    inventory: {
+        tab: 'store-tab-inventory',
+        title: 'inventory-title'
+    },
+
+    catalog: {
+        tab: 'store-tab-catalog',
+        title: 'catalog-title'
+    },
+
+    cart: {
+        tab: 'store-tab-cart',
+        title: 'cart-title'
+    },
+
+    payments: {
+        tab: 'store-tab-payments',
+        title: 'payment-title'
+    },
+
+    orders: {
+        tab: 'store-tab-orders',
+        title: 'orders-title'
+    }
+
+};
