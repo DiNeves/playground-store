4009b7d5f2aa621dd50f2846665fce9ba147dfbc
Tatiana Taketsuma <tatiana.taketsuma@mindera.com>
2025-11-23 20:54:45 +0000

    Preventing adding out of stock products into the cart

--- tests/specs/catalog.spec.js
+++ tests/specs/catalog.spec.js
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 import { StoreMainPage } from '../pages/store.page';
 import { CatalogPage } from '../pages/catalog.page';
 
+
 test.describe('Catalog section', () => {
 
     test.beforeEach(async ({ page }) => {
@@ -24,4 +25,15 @@ test.describe('Catalog section', () => {
         await catalogPage.addProductToCart('Lightsaber (Star Wars)');
     });
 
+    /**
+     * Scenario 2: Prevent adding out-of-stock items
+     * 
+     * Given an item has quantity 0
+     * Then the button should display "Out of Stock"
+     * And the button should be disabled
+    */
+    test('Validate "Out of Stock" state for all displayed Products', async ({ page }) => {
+        const catalogPage = new CatalogPage(page);
+        await catalogPage.validateOutOfStockButtonInAllItems();
+    });
 });
