5e847cdc12a18fb716349c5585be605995d41996
Tatiana Taketsuma <tatiana.taketsuma@mindera.com>
2025-11-24 19:39:10 +0000

    Payment purchase complete

--- tests/specs/payments.spec.js
+++ tests/specs/payments.spec.js
@@ -1,5 +1,11 @@
 import { test } from '@playwright/test';
 import { StoreMenuPage } from '../pages/storeMenu.page'; 
+import { PaymentPage } from '../pages/payments.page';
+import { InventoryPage } from '../pages/inventory.page';
+import { CatalogPage } from '../pages/catalog.page';
+import { CartPage } from '../pages/cart.pages';
+import { PAYMENTSLOCATORS } from '../data/payments.data';
+import { STOREPRODUCTS } from '../data/storeMenu.data';
 
 test.describe('Payments section', () => {
 
@@ -23,4 +29,37 @@ test.describe('Payments section', () => {
         // await storeMenuPage.navigateToHomeTab();
     });
 
-});
+    /**
+     * Scenario 2: Complete a purchase
+     * 
+     * Given I selected a payment method
+     * When I click "Confirm Payment"
+     * Then the order should be created
+     * And I should be redirected to the Orders page
+    */
+    const methods = Object.keys(PAYMENTSLOCATORS.paymentMethods);
+    for (const method of methods) {
+        test(`Complete a Purchase using ${method}`, async ({ page }) => {
+            const storeMenuPage = new StoreMenuPage(page);
+            const paymentPage = new PaymentPage(page);
+            const inventoryPage = new InventoryPage(page);
+            const catalogPage = new CatalogPage(page);
+            const cartPage = new CartPage(page);
+
+            const product = STOREPRODUCTS[0];
+
+            await test.step('Pre-Conditions: Create Product - Add to Cart - Go to Payment', async () => {
+                await storeMenuPage.navigateToInventoryTab();
+                await inventoryPage.fillProductRequiredFields(product);
+                await inventoryPage.clickAddProductButton();
+                await storeMenuPage.navigateToCatalogTab();
+                await catalogPage.addProductToCart(product.name);
+                await storeMenuPage.navigateToCartTab();
+                await cartPage.clickGoToPaymentButton();
+            });
+            await paymentPage.selectPaymentMethod(method);
+            await paymentPage.clickConfirmPaymentButton();
+            await paymentPage.validateRedirectToOrdersPage();
+        });
+    }
+  });
