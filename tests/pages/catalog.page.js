abe116c1833c4659915617feb2c9804dd27941f0
DiNeves <dms.neves@gmail.com>
2025-11-24 23:09:37 +0000

    Add test display items in cart page in storeFlow

--- tests/pages/catalog.page.js
+++ tests/pages/catalog.page.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { STORELOCATORS } from '../data/storeMenu.data'; 
+import { STORELOCATORS } from '../data/storeMenu.data';
 import { CATALOGLOCATORS } from '../data/catalog.data';
 
 export class CatalogPage {
@@ -11,16 +11,16 @@ export class CatalogPage {
 
         this.table = page.getByTestId(CATALOGLOCATORS.table.list);
         this.tableListItems = this.table.getByRole(CATALOGLOCATORS.table.listItem);
-        this.tableProduct = (product) =>  this.tableListItems.filter({ hasText: `${product}` });
-        
-        this.addToCartButton = (product) =>  this.tableProduct(product).getByRole('button', { name: CATALOGLOCATORS.table.addToCartButton });
+        this.tableProduct = (product) => this.tableListItems.filter({ hasText: `${product}` });
+
+        this.addToCartButton = (product) => this.tableProduct(product).getByRole('button', { name: CATALOGLOCATORS.table.addToCartButton });
 
         this.productQuantity = (product) => this.tableProduct(product).locator("[data-testid^=" + CATALOGLOCATORS.table.productQuantity + "]");
-   
+
     }
 
     async addProductToCart(product) {
-        
+
         const initialQuantity = Number((await this.productQuantity(product).textContent()).replace(' units', ''));
 
         await test.step('Click on Add to Cart button', async () => {
@@ -28,7 +28,25 @@ export class CatalogPage {
         });
         await test.step('Validate if quantity decreased on selected product', async () => {
             const finalQuantity = Number((await this.productQuantity(product).textContent()).replace(' units', ''));
-            await expect(finalQuantity).toBe(initialQuantity-1);
+            await expect(finalQuantity).toBe(initialQuantity - 1);
+        });
+    }
+
+    async addProductToCartMultipleTimes(product) {
+        await test.step('Add a product multiple times to cart', async () => {
+            if (product.addedQuantity > 0) {
+                for (let i = 0; i < product.addedQuantity; i++) {
+                    await this.addProductToCart(product.name);
+                }
+            };
+        });
+    }
+
+    async addProductListToCart(productsList) {
+        await test.step('Add product list to cart', async () => {
+            for (const product of productsList) {
+                await this.addProductToCartMultipleTimes(product);
+            };
         });
     }
 
@@ -36,20 +54,20 @@ export class CatalogPage {
         return this.productQuantity(product);
     }
 
-    async validateOutOfStockButtonInAllItems(){
+    async validateOutOfStockButtonInAllItems() {
         await test.step('Validate Out of Stock button and disabled state for all out of stock products', async () => {
             const allProducts = await this.tableListItems.all();
-            for (const product of allProducts){
+            for (const product of allProducts) {
                 const unitsLocator = product.locator('text=units');
                 await unitsLocator.click();
                 const text = await unitsLocator.textContent();
                 const stockQuantity = parseInt(text);
                 const button = product.getByRole('button');
 
-                if (stockQuantity === 0){
+                if (stockQuantity === 0) {
                     await expect(button).toHaveText('Out of Stock');
                     await expect(button).toBeDisabled();
-                } else{
+                } else {
                     await expect(button).toHaveText('Add to Cart');
                     await expect(button).toBeEnabled();
                 }
--- tests/specs/storeFlow.spec.js
+++ tests/specs/storeFlow.spec.js
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
-import { STOREPRODUCTS } from '../data/storeMenu.data'; 
-import { StoreMenuPage } from '../pages/storeMenu.page'; 
+import { STOREPRODUCTS } from '../data/storeMenu.data';
+import { StoreMenuPage } from '../pages/storeMenu.page';
 import { InventoryPage } from '../pages/inventory.page';
 import { CatalogPage } from '../pages/catalog.page';
 import { CartPage } from '../pages/cart.pages';
@@ -29,14 +29,36 @@ test.describe('Integration tests between store pages', () => {
         const cartPage = new CartPage(page);
 
         const product = STOREPRODUCTS[0];
-        await inventoryPage.fillProductRequiredFields(product);
-        await inventoryPage.clickAddProductButton();
-        await inventoryPage.validateProductAddedOnTable(product);
+        await inventoryPage.addAndValidateProduct(product);
 
         await storeMenuPage.navigateToCatalogTab();
         await catalogPage.addProductToCart(product.name);
 
         await storeMenuPage.navigateToCartTab();
         await cartPage.validateProductSuccessfullyAddedtoCart(product);
+    });
+
+    /**
+     * Scenario 2: Display cart items and totals in cart page
+     * 
+     * Given I have added items to the cart
+     * When  I visit the Cart page
+     * Then  each item name, quantity, and subtotal should be displayed
+     * And   the total amount should be correctly calculated.
+    */
+    test('Display cart items and totals in cart page', async ({ page }) => {
+        const storeMenuPage = new StoreMenuPage(page);
+        const inventoryPage = new InventoryPage(page);
+        const catalogPage = new CatalogPage(page);
+        const cartPage = new CartPage(page);
+
+        await inventoryPage.addMultipleProducts(STOREPRODUCTS);
+        await storeMenuPage.navigateToCatalogTab();
+
+        await catalogPage.addProductListToCart(STOREPRODUCTS);
+        await storeMenuPage.navigateToCartTab();
+
+        await cartPage.validateProductListAddedtoCart(STOREPRODUCTS);
+        await cartPage.validateTotalCartPrice();
     });
 });
